3dc29c110d10d4ad25016b57561388bfa17bfaa5
Chaiyasit Lertkultanon <chaiyasit.ler@gmail.com>
2021-10-19 10:31:47 +0700

    add picture resizing method in model by PIL

--- network/models.py
+++ network/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
+from PIL import Image
 
 class User(AbstractUser):
     def __str__(self):
@@ -14,6 +15,15 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username}'s Profile"
 
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+    
     def serialize(self, user):
         return {
             "id": self.user.id,
